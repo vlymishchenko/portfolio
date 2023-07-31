256d6bb6b73551e4bcde0d9c20fbf8b549369c24
vlymishchenko <51797420+vlymishchenko@users.noreply.github.com>
2023-07-31 10:06:21 +0300

    Update Cocktails_Ingredient_Raffle.py

--- Cocktails_Ingredient_Raffle.py
+++ Cocktails_Ingredient_Raffle.py
@@ -1,42 +1,13 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyOB9CRks+dc4HNbo2jlHMhh"
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "code",
-      "execution_count": null,
-      "metadata": {
-        "id": "jZJWAV1lvOXi"
-      },
-      "outputs": [],
-      "source": [
-        "IngrCocktail = {'aperol' : ['prosecco', 'aperol', 'soda', 'orange', 'ice'],\n",
-        "                'espressomartini' : ['vodka', 'liquor', 'ice'],\n",
-        "                                     'negroni' : ['soda', 'aperol', 'ice']}\n",
-        "\n",
-        "def CocktailGenerator(Ingridients):\n",
-        "  IngridientsList = Ingridients.split(', ')\n",
-        "  # print(IngridientsList)\n",
-        "  for i in IngrCocktail:\n",
-        "    # print(set(IngrCocktail[i]), set(IngridientsList)), len(IngrCocktail[i])\n",
-        "    if len(set(IngrCocktail[i]) & set(IngridientsList)) == len(IngrCocktail[i]):\n",
-        "      print(i, IngrCocktail[i])\n",
-        "\n",
-        "CocktailGenerator('prosecco, aperol, soda, orange, ice')"
-      ]
-    }
-  ]
-}
+IngrCocktail = {'aperol' : ['prosecco', 'aperol', 'soda', 'orange', 'ice'],
+                'espressomartini' : ['vodka', 'liquor', 'ice'],
+                                     'negroni' : ['soda', 'aperol', 'ice']} #you can just add more cocktails there
+
+def CocktailGenerator(Ingridients):
+  IngridientsList = Ingridients.split(', ')
+  # print(IngridientsList)
+  for i in IngrCocktail:
+    # print(set(IngrCocktail[i]), set(IngridientsList)), len(IngrCocktail[i])
+    if len(set(IngrCocktail[i]) & set(IngridientsList)) == len(IngrCocktail[i]):
+      print(i, IngrCocktail[i])
+
+CocktailGenerator('prosecco, aperol, soda, orange, ice')
